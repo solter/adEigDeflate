164087d93e4388ca3aa39ca6fd8c5c4455a92b97
Peter Solfest <pmsolfest@gmail.com>
2014-01-21 18:49:59 -0500

    fixed bugs in tests

--- src/testing.m
+++ src/testing.m
@@ -1,43 +1,61 @@
-n = 1000;
-mkdir('../zeroPlots')
+n = 10;
 vals = nan(n,2);
-for er = [nan, -4:.5:-1]
-  parfor i=1:n
-    A = 1 - 2*rand(140);
-    ev = 1 - 2*rand(140,1);
-    A = inv(A)*diag(ev)*A;
-    A = hess(A);
-    
-    #shifts 
-    if(er != nan)#varied from evals
-      sft = 1 - 2*rand(14,1);
-      sft *= 10^er;
-      sft += 1;
-      sft = sft .* ev(1:14);
-    else#which are evals
-      sft = ev(1:14);
-    endif
-
-
-    [H,info] = trainBust(A, sft,false,false,false);
-    vals(i,1) = info.nz;
-    #vals(i,2) = norm(sort(real(eig(H))) - sort(ev));
-    if(mod(i,100) == 0)
-      printf("\n%d%% finished wih er = %.1f\n",i/10,er);
-      fflush(stdout);
-    elseif(mod(i,10) == 0)
-      printf(".");
-      fflush(stdout);
-    endif
-  endparfor
-  figure();
-  hist(vals(:,1),min(vals(:,1)):max(vals(:,1)),100);
-  ylabel("%")
-  if(er != nan)
-    title(sprintf("Num 0's with %.2f %% in evals", 100*10^(-er)))
-    print('../zeroPlots/1e%.1f.png',er)
+for tomid = [true, false]
+  dirt = '';
+  if(tomid)
+    dirt = '../zeroMidPlots';
   else
-    title("Num 0's with exact eigenvalues")  
-    print('../zeroPlots/exact.png')
+    dirt = '../zeroBotPlots';
   endif
+  mkdir(dirt);
+  er = nan;
+  do#do while still getting holes in spike
+    for i=1:n#for each sample
+      A = 1 - 2*rand(140);
+      ev = 1 - 2*rand(140,1);
+      A = inv(A)*diag(ev)*A;
+      A = hess(A);
+     
+      sft = zeros(14,1);
+      #shifts 
+      if(!isnan(er))#varied from evals
+        sft = 1 - 2*rand(14,1);
+        sft *= 10^er;
+        sft += 1;
+        sft = sft .* ev(1:14);
+      else#which are evals
+        sft = ev(1:14);
+      endif
+
+
+      [H,info] = trainBust(A, sft,false,false,tomid);
+      vals(i,1) = info.nz;
+      #vals(i,2) = norm(sort(real(eig(H))) - sort(ev));
+      if(mod(i,floor(n/10)) == 0)
+        printf("\n%.1f%% finished wih er = %.1f\n",100*i/n,er);
+        fflush(stdout);
+      elseif(mod(i,10) == 0)
+        printf(".");
+        fflush(stdout);
+      endif
+    endfor
+    figure();
+    hist(vals(:,1),min(vals(:,1)):max(vals(:,1))+1,100);
+    ylabel("%")
+    if(!isnan(er))
+      title(sprintf("Num 0's with %.1g variation in evals", 10^(er)))
+      print(sprintf('%s/1e%.1f.png',dirt,er))
+      close all
+    else
+      title("Num 0's with exact eigenvalues")  
+      print(sprintf('%s/exact.png',dirt))
+      close all
+      er = -9;
+    endif
+    printf("\nDone with error = %d\n",er);
+    fflush(stdout);
+    er++;
+  until(sum(vals(:,1)) == 0)
+  printf("\n\nDone with 1 cycle of tomid\n");
+  fflush(stdout)
 endfor
